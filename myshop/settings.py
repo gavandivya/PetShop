08e5946bad8f49803cfc334106dbdc1548eec671
Divya gavandi <49062060+gavandivya@users.noreply.github.com>
2020-07-03 12:33:03 +0530

    Changes

--- myshop/settings.py
+++ myshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '64g($!oyf&nayoe10gd6y0q_dk-i&4-t6a7i_mp(y^3h9#4iif'
+SECRET_KEY = '#############################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
